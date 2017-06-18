f098a413e109d870384b6efd606e42c1ec5f1922
texmex <josefsahlberg+minetest@gmail.com>
2017-06-18 21:11:07 +0200

    Replace deprecated settings method

--- init.lua
+++ init.lua
@@ -8,19 +8,19 @@ distributed without any warranty.
 ]]
 
 --Configuration variables, these are all explained in README.md
-SPRINT_KEY = minetest.setting_getbool("sprint_key")
+SPRINT_KEY = minetest.settings:get("sprint_key")
 if SPRINT_KEY == nil then SPRINT_KEY = "e" end
 
-SPRINT_SPEED = minetest.setting_getbool("sprint_speed")
+SPRINT_SPEED = minetest.settings:get("sprint_speed")
 if SPRINT_SPEED == nil then SPRINT_SPEED = 1.8 end
 
-SPRINT_JUMP = minetest.setting_getbool("sprint_jump")
+SPRINT_JUMP = minetest.settings:get("sprint_jump")
 if SPRINT_JUMP == nil then SPRINT_JUMP = 1.1 end
 
-SPRINT_STAMINA = minetest.setting_getbool("sprint_stamina")
+SPRINT_STAMINA = minetest.settings:get("sprint_stamina")
 if SPRINT_STAMINA == nil then SPRINT_STAMINA = 20 end
 
-SPRINT_TIMEOUT = minetest.setting_getbool("sprint_timeout") --Only used if SPRINT_KEY = "w"
+SPRINT_TIMEOUT = minetest.settings:get("sprint_timeout") --Only used if SPRINT_KEY = "w"
 if SPRINT_TIMEOUT == nil then SPRINT_TIMEOUT = 0.5 end
 
 if minetest.get_modpath("hudbars") ~= nil then
